9835149adb4aa5f143130af39f64455c517d423d
Jaume Llort <jaume.llort@quantion.com>
2023-05-19 12:55:41 +0200

    Comentarios en metodos controller

--- src/main/java/com/quantion/ejercicio/controller/UserRestController.java
+++ src/main/java/com/quantion/ejercicio/controller/UserRestController.java
@@ -22,6 +22,11 @@ public class UserRestController {
 	@Autowired
 	UserService userService;
 
+	/**
+	 * Method to retrieve all the users of the application
+	 * 
+	 * @return Users
+	 */
 	@GetMapping("/users")
 	public ResponseEntity<List<User>> getUsers() {
 
@@ -29,6 +34,12 @@ public class UserRestController {
 
 	}
 
+	/**
+	 * Method to save a new user
+	 * 
+	 * @param user User to be created
+	 * @return Created user
+	 */
 	@PostMapping("/users")
 	public ResponseEntity<User> saveUser(@RequestBody User user) {
 		user = userService.save(user);
@@ -39,12 +50,25 @@ public class UserRestController {
 		}
 	}
 
+	/**
+	 * Method to delete a user
+	 * 
+	 * @param id Id of the user to be deleted
+	 * @return
+	 */
 	@DeleteMapping("/users/{id}")
 	public ResponseEntity<Boolean> deleteUser(@PathVariable("id") Long id) {
 		userService.deleteUser(id);
 		return new ResponseEntity<>(true, HttpStatus.OK);
 	}
 
+	/**
+	 * Method to update the info of an user
+	 * 
+	 * @param id Id of the user to be updated
+	 * @param user User
+	 * @return The updated user
+	 */
 	@PutMapping("/users/{id}")
 	public ResponseEntity<User> updateUser(@PathVariable("id") Long id, @RequestBody User user) {
 
